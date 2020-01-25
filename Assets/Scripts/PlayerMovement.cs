9a3ed1a72a906b0c9fa29edf680b16bf86c3f54f
ChiD12 <daniel.chid@gmail.com>
2020-01-25 12:44:53 -0500

    some changes

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float x;
+    public float y;
+    public float z;
+    Vector3 move;
+    void Start()
+    {
+        move = new Vector3(x,y,z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = move;
+    }
+}
