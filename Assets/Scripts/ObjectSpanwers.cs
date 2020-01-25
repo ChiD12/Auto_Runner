4eeb89dad987061fb2c07071152545260016b26b
Armando Russo <russo_449@hotmail.com>
2020-01-25 17:40:06 -0500

    Random Spawning Done

--- Assets/Scripts/ObjectSpanwers.cs
+++ Assets/Scripts/ObjectSpanwers.cs
@@ -10,33 +10,33 @@ public class ObjectSpanwers : MonoBehaviour
     //used for the area where it will spawn in
     public Vector3 size;
     public Vector3 center;
-
+    public int waitSpawn; //wait x amount of time before begin spawning objects
+    public int waitMin; //least amount of wait time
+    public int waitMax; //max amount of wait time
+    public int waitTime; //interval when objects spawn in
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-
-        SpawnObjects();
-
+        StartCoroutine(spawnWait());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.Q)) {
-            SpawnObjects();
-        }
-    }
+        waitTime = Random.Range(waitMin, waitMax); //constantly making a new float value of time within this range
 
-    public void SpawnObjects() //spawns the random flying objects (projectiles, crates, etc)
-    {
-        Vector3 spawnPos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.z / 2, size.z / 2)); //this gets the exact coordinates that ranges in the cube FOR each axis
-
-        Instantiate(Projectileprefab, spawnPos, Quaternion.identity); //this will spawn the actual object which will be within the randomly generated position 
-    
     }
+    /*
+        public void SpawnObjects() //spawns the random flying objects (projectiles, crates, etc)
+        {
+            Vector3 spawnPos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.z / 2, size.z / 2)); //this gets the exact coordinates that ranges in the cube FOR each axis
+
+            Instantiate(Projectileprefab, spawnPos, Quaternion.identity); //this will spawn the actual object which will be within the randomly generated position 
+
+        }*/
 
 
     void OnDrawSpawn() //this is the shape that is used to spawn objects into (i.e, where random objects will spawn)
@@ -45,5 +45,20 @@ public class ObjectSpanwers : MonoBehaviour
 
         Gizmos.DrawCube(center, size); //draw the cube using these parameters size and center
 
+    }
+
+    IEnumerator spawnWait()
+    {
+        yield return new WaitForSeconds(waitSpawn); //waits for x amount of time before spawning
+
+        while(true)
+        {
+
+            Vector3 spawnPos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), 0, Random.Range(-size.z / 2, size.z / 2)); //this gets the exact coordinates that ranges in the cube FOR each axis
+
+            Instantiate(Projectileprefab, spawnPos, Quaternion.identity); //this will spawn the actual object which will be within the randomly generated position 
+
+            yield return new WaitForSeconds(waitSpawn);
+        }
     }
 }
